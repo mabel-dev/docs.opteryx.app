d00f74e80443923a921487a6047c061f8eabeb10
joocer <justin.joyce@joocer.com>
2025-12-27 15:13:21 +0000

    df

---
+++ docs-site/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Page(){
+  return (
+    <main>
+      <h1>Welcome to Opteryx Docs</h1>
+      <p>This is a minimal Next.js docs demo.</p>
+      <ul>
+        <li><a href="/quick-start">Quick start</a></li>
+      </ul>
+    </main>
+  )
+}
---
+++ docs-site/app/quick-start/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function QuickStart(){
+  return (
+    <main>
+      <h1>Quick start</h1>
+      <p>Run the following locally:</p>
+      <pre><code>npm install
+npm run dev</code></pre>
+    </main>
+  )
+}
