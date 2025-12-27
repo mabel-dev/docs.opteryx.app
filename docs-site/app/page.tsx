0454bc1251de857a2ee9a3ab16eda7c5f219ecea
joocer <justin.joyce@joocer.com>
2025-12-27 16:26:28 +0000

    chore(site): redirect root to /docs and include migrated docs

--- docs-site/app/page.tsx
+++ docs-site/app/page.tsx
@@ -1,13 +1,6 @@
-import React from 'react'
+import { redirect } from 'next/navigation'
 
 export default function Page(){
-  return (
-    <main>
-      <h1>Welcome to Opteryx Docs</h1>
-      <p>This is a minimal Next.js docs demo.</p>
-      <ul>
-        <li><a href="/quick-start">Quick start</a></li>
-      </ul>
-    </main>
-  )
+  // Redirect root to the migrated docs index
+  redirect('/docs')
 }
